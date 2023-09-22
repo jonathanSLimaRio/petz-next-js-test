ca2f76076636b288823f2aad80c6e08571286f97
jonathanSLimaRio <jonathan.planeje@gmail.com>
2023-09-22 16:55:34 -0300

    feat: add mantine to style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
+import { MantineProvider, createTheme } from '@mantine/core';
+import '@mantine/core/styles.css';
 import type { AppProps } from "next/app";
 import Head from "next/head";
+
 import "../styles/globals.css";
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -11,7 +18,9 @@ export default function App({ Component, pageProps }: AppProps) {
           rel="stylesheet"
         />
       </Head>
+      <MantineProvider theme={theme}>
       <Component {...pageProps} />
+      </MantineProvider>
     </>
   );
 }
