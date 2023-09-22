b4cef25ceb9b7f72d8bd88e17c72b89c0b12a84f
jonathanSLimaRio <jonathan.planeje@gmail.com>
2023-09-22 16:41:57 -0300

    feat: Start project test

--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -1,13 +1,13 @@
-import { Title, Text, Anchor } from '@mantine/core';
+import { Anchor, Text, Title } from '@mantine/core';
 import classes from './Welcome.module.css';
 
 export function Welcome() {
   return (
     <>
       <Title className={classes.title} ta="center" mt={100}>
-        Welcome to{' '}
+        Test{' '}
         <Text inherit variant="gradient" component="span" gradient={{ from: 'pink', to: 'yellow' }}>
-          Mantine
+        PETZ
         </Text>
       </Title>
       <Text color="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
