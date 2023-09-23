7db615713ba45db7980c67e19895acfb61bea6d7
jonathanSLimaRio <jonathan.planeje@gmail.com>
2023-09-23 02:21:19 -0300

    feat: API integrations

--- pages/api/pokemon/api.ts
+++ pages/api/pokemon/api.ts
@@ -1,5 +1,23 @@
-export const fetchPokemonLocations = async () => {
-  const res = await fetch('https://pokeapi.co/api/v2/location/');
+export const fetchPokemonRegion = async () => {
+  const res = await fetch('https://pokeapi.co/api/v2/region/');
+  if (!res.ok) {
+    throw new Error(`Failed to fetch locations: ${res.status}`);
+  }
+  const data = await res.json();
+  return data.results;
+}
+
+export const fetchPokemonCities = async (city: string = 'kanto') => {
+  const res = await fetch(`https://pokeapi.co/api/v2/region/${city}`);
+  if (!res.ok) {
+    throw new Error(`Failed to fetch locations: ${res.status}`);
+  }
+  const data = await res.json();
+  return data.locations;
+}
+
+export const fetchPokemons = async () => {
+  const res = await fetch('https://pokeapi.co/api/v2/pokemon/');
   if (!res.ok) {
     throw new Error(`Failed to fetch locations: ${res.status}`);
   }
--- pages/schedule/index.tsx
+++ pages/schedule/index.tsx
@@ -1,20 +1,44 @@
-import { useEffect, useState } from "react";
-import { fetchPokemonLocations } from "../api/pokemon/api";
 import {
-  fetchAvailableDates,
-  fetchAvailableTimes,
-} from "../api/utils/api";
+  Button,
+  Container,
+  Grid,
+  Group,
+  Select,
+  Text,
+  TextInput,
+  Title,
+} from "@mantine/core";
+import { IconArrowDown } from "@tabler/icons-react";
+import { useEffect, useState } from "react";
+import {
+  fetchPokemonCities,
+  fetchPokemonRegion,
+  fetchPokemons,
+} from "../api/pokemon/api";
+import { fetchAvailableDates, fetchAvailableTimes } from "../api/utils/api";
 
-interface IRegion {
+interface IRegionCityPokemon {
   name: string;
+  url: string;
 }
+
 type DateType = string;
 type TimeType = string;
 
-export default function Home() {
+export default function Schedule() {
   const [availableDates, setAvailableDates] = useState<DateType[]>([]);
   const [availableTimes, setAvailableTimes] = useState<TimeType[]>([]);
-  const [regions, setRegions] = useState<IRegion[]>([]);
+  const [regions, setRegions] = useState<IRegionCityPokemon[]>([]);
+  const [cities, setCities] = useState<IRegionCityPokemon[]>([]);
+  const [pokemons, setPokemons] = useState<IRegionCityPokemon[]>([]);
+
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [regionForm, setRegionForm] = useState("");
+  const [cityForm, setCityForm] = useState("");
+  const [pokemonForm, setPokemonForm] = useState("");
+
+  console.log(pokemons, "pokemons");
 
   useEffect(() => {
     async function fetchData() {
@@ -25,8 +49,11 @@ export default function Home() {
         const times = await fetchAvailableTimes(dates[0]);
         setAvailableTimes(times);
 
-        const regionsData = await fetchPokemonLocations();
-        setRegions(regionsData);
+        const regionData = await fetchPokemonRegion();
+        setRegions(regionData);
+
+        const pokemonData = await fetchPokemons();
+        setPokemons(pokemonData);
       } catch (error) {
         console.error("There was an error fetching the data:", error);
       }
@@ -34,29 +61,83 @@ export default function Home() {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    async function fetchDataCity() {
+      try {
+        const cityData = await fetchPokemonCities(regionForm);
+        setCities(cityData);
+      } catch (error) {
+        console.error("There was an error fetching the data:", error);
+      }
+    }
+    fetchDataCity();
+  }, [regionForm]);
+
   return (
-    <div>
-      <h1>Scheduling</h1>
-      <h2>Available Dates</h2>
-      <ul>
-        {availableDates.map((date, index) => (
-          <li key={index}>{date}</li>
-        ))}
-      </ul>
-
-      <h2>Available Times</h2>
-      <ul>
-        {availableTimes.map((time, index) => (
-          <li key={index}>{time}</li>
-        ))}
-      </ul>
-
-      <h2>Pokémon Regions</h2>
-      <ul>
-        {regions.map((region) => (
-          <li key={region.name}>{region.name}</li>
-        ))}
-      </ul>
-    </div>
+    <Container fluid>
+      <Title order={3}>
+        Preencha o formulário abaixo para agendar sua consulta
+      </Title>
+      <Container>
+        <Group grow>
+          <TextInput
+            radius="md"
+            label="Nome"
+            placeholder="Digite seu nome"
+            value={name}
+            onChange={(event) => setName(event.currentTarget.value)}
+          />
+          <TextInput
+            radius="md"
+            label="Sobrenome"
+            placeholder="Digite seu sobrenome"
+            value={lastName}
+            onChange={(event) => setLastName(event.currentTarget.value)}
+          />
+        </Group>
+        <Group grow>
+          <Select
+            label="Região"
+            placeholder="Selecione sua região"
+            data={regions?.map((region) => region.name)}
+            value={regionForm}
+            onChange={setRegionForm as any}
+            allowDeselect
+            mt="md"
+          />
+          <Select
+            label="Cidade"
+            placeholder="Selecione sua cidade"
+            data={cities?.map((city) => city.name) || []}
+            value={cityForm}
+            onChange={setCityForm as any}
+            allowDeselect
+            mt="md"
+          />
+        </Group>
+        <Grid grow>
+          <Grid.Col span={12}>
+            <Text fw={500}>Cadastre seu time</Text>
+            <Text c="dimmed">Atendemos até 06 pokémons por vez</Text>
+          </Grid.Col>
+          <Grid.Col span={12}>
+            <Group>
+              <Text c="dimmed">Pokémon {`0` + 0}</Text>
+              <Select
+                placeholder="Selecione seu pokémon"
+                data={pokemons?.map((pokemonItem) => pokemonItem?.name) || []}
+                value={pokemonForm}
+                onChange={setPokemonForm as any}
+                allowDeselect
+                mt="md"
+              />
+            </Group>
+          </Grid.Col>
+        </Grid>
+        <Button mt="md" variant="outline" color="gray" rightSection={<IconArrowDown size={14} />}>
+          Adicionar novo pokémon ao time...
+        </Button>
+      </Container>
+    </Container>
   );
 }
