8ecb4ba69de079cd0fc17656f841f8592a92cc31
jonathanSLimaRio <jonathan.planeje@gmail.com>
2023-09-25 11:12:30 -0300

    fix: clean code api

--- pages/api/pokemon/api.ts
+++ pages/api/pokemon/api.ts
@@ -1,26 +1,28 @@
+const BASE_API = "https://pokeapi.co/api/v2/";
+
 export const fetchPokemonRegion = async () => {
-  const res = await fetch('https://pokeapi.co/api/v2/region/');
+  const res = await fetch(`${BASE_API}region/`);
   if (!res.ok) {
     throw new Error(`Failed to fetch locations: ${res.status}`);
   }
   const data = await res.json();
   return data.results;
-}
+};
 
-export const fetchPokemonCities = async (city: string = 'kanto') => {
-  const res = await fetch(`https://pokeapi.co/api/v2/region/${city}`);
+export const fetchPokemonCities = async (city: string = "kanto") => {
+  const res = await fetch(`${BASE_API}region/${city}`);
   if (!res.ok) {
     throw new Error(`Failed to fetch locations: ${res.status}`);
   }
   const data = await res.json();
   return data.locations;
-}
+};
 
 export const fetchPokemons = async () => {
-  const res = await fetch('https://pokeapi.co/api/v2/pokemon/');
+  const res = await fetch(`${BASE_API}pokemon/`);
   if (!res.ok) {
     throw new Error(`Failed to fetch locations: ${res.status}`);
   }
   const data = await res.json();
   return data.results;
-}
+};
